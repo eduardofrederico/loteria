989942a6aa67e64f41f73b09fd4f3f435e66d122
Eduardo Frederico <eduardo.frederico@icloud.com>
2023-04-02 20:00:45 -0300

    Initial Commit

---
+++ Loteria/LoteriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoteriaApp.swift
+//  Loteria
+//
+//  Created by Eduardo Frederico on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LoteriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
